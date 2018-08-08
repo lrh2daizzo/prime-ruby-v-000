7ee2f9596b7f1978f308161887e0c07e7721428c
Luis Herrera <herrera.lrh@gmail.com>
2018-08-08 23:06:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,9 @@ require 'pry'
 def prime?(int)
   if int < 0 or int == 0 or int == 1
     return false
+  else
+    (2..int-1).to_a.all? do |possible_factor|
+      int % possible_factor != 0
+    end
   end
 end
